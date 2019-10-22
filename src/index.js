d8a4263078f99dd7c9444bc03594a4ba27224209
Roger Lee <rmlee0003@gmail.com>
2019-10-22 19:42:01 -0400

    initial redux flow setup in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { createStore, cokmbineReducers, combineReducers } from 'redux'
+import {Provider} from "react-redux"
+import {reducer as addReducer } from "./reducers/addFeature"
+import {reducer as removeReducer } from "./reducers/removeFeature"
+import reducer from './reducers/addFeature'
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const rootReducer = combineReducers({
+ add: addReducer,
+ remove: removeReducer
+})
+
+const store = createStore(rootReducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+ <App />
+ </Provider>
+ , rootElement);
